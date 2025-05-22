d7adc6bb08479cc14e40a74914533e74cec99cc4
Justin White <jw@justinwhite.net>
2025-05-22 16:01:01 -0400

    Update db init script

--- init_students.sql
+++ init_students.sql
@@ -1,10 +1,13 @@
 DROP TABLE students;
 
-CREATE TABLE IF NOT EXISTS students (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(256) NOT NULL,
     grade INTEGER NOT NULL
 );
 
 INSERT INTO students (name, grade) VALUES('Alice', 90);
 INSERT INTO students (name, grade) VALUES('Bob', 95);
+INSERT INTO students (name, grade) VALUES('Marty', 88);
+INSERT INTO students (name, grade) VALUES('David', 88);
+INSERT INTO students (name, grade) VALUES('Wayne', 99);
