427ac37f881190896650e4d8c6988b003f7b02f9
Justin White <jw@justinwhite.net>
2025-05-21 23:53:26 -0400

    Remove spec comment

--- main.go
+++ main.go
@@ -139,21 +139,3 @@ func deleteStudent(ID int) Student {
 
 	return deletedStudent
 }
-
-/*
-Back-end: Implement a RESTful API with CRUD operations for managing students.
-    API
-        GET /students - Retrieve a list of all students.
-        POST /students - Add a new student.
-        PUT /students/:id - Update an existing student.
-        DELETE /students/:id - Delete a student.
-    Data Model
-        id (integer, primary key)
-        name (string)
-        grade (integer)
-Front-end: Create a functional UI that interacts with the backend API. It does not need to be polished or aesthetically pleasing; it just needs to be able to connect with the back-end and perform the following functions:
-    View a list of all students.
-    Add a new student.
-    Update an existing student.
-    Delete a student.
-*/
