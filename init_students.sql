9bb66888add51ec22cfcc362ad8becf33561a790
Justin White <jw@justinwhite.net>
2025-05-23 14:54:59 -0400

    split db init script and test data script

--- init_students.sql
+++ init_students.sql
@@ -8,10 +8,4 @@ CREATE TABLE students (
     grade INTEGER NOT NULL
 );
 
-INSERT INTO students (name, grade) VALUES('Alice', 90);
-INSERT INTO students (name, grade) VALUES('Bob', 95);
-INSERT INTO students (name, grade) VALUES('Marty', 88);
-INSERT INTO students (name, grade) VALUES('David', 88);
-INSERT INTO students (name, grade) VALUES('Wayne', 99);
-
 COMMIT;
---
+++ test_data.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+INSERT INTO students (name, grade) VALUES('Alice', 90);
+INSERT INTO students (name, grade) VALUES('Bob', 95);
+INSERT INTO students (name, grade) VALUES('Marty', 88);
+INSERT INTO students (name, grade) VALUES('David', 88);
+INSERT INTO students (name, grade) VALUES('Wayne', 99);
+
+COMMIT;
